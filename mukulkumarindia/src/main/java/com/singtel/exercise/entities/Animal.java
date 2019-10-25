824c11d7417f10812191c09437b6d89627fa704f
Mukul Kumar <mukulkumar@Mukuls-MacBook-Pro.local>
2019-10-25 07:42:43 +0400

    first commit

---
+++ mukulkumarindia/src/main/java/com/singtel/exercise/entities/Animal.java
@@ -0,0 +1,6 @@
+package com.singtel.exercise.entities;
+
+public class Animal {
+    void walk(){
+        System.out.println("I am walking"); }
+}
---
+++ mukulkumarindia/src/main/java/com/singtel/exercise/entities/Bird.java
@@ -0,0 +1,11 @@
+package com.singtel.exercise.entities;
+
+public class Bird extends Animal{
+    void fly() {
+    System.out.println("I am flying");
+    }
+
+    void sing() {
+        System.out.println("I am Singing");
+    }
+}
